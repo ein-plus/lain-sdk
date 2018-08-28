7c6e5f9deb03e38e0eb26ce908676484ce727095
sunyi00 <sunyi00@gmail.com>
2018-08-28 10:30:36 +0800

    update req and bump version

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from lain_sdk import __version__
 requirements = [
     'six>=1.9.0',
     'future>=0.16.0',
-    'Jinja2==2.7.3',
+    'Jinja2>=2.7.3',
     'PyYAML>=3.12',
     'enum34;python_version<"3.4"',
     'requests>=2.6.1',
-    'docker==2.6.1',
-    'jsonschema==2.5.1',
+    'docker>=2.6.1',
+    'jsonschema>=2.5.1',
 ]
 
 
